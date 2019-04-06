24311519bf5a5f760261d0c78ee67b41d832c496
Yufei <46723525+YufeiT@users.noreply.github.com>
2019-04-05 17:34:16 -0700

    Update player.cpp

--- player.cpp
+++ player.cpp
@@ -1,8 +1,7 @@
 // ************ PLAYER DEFINITIONS *****************    
 //
-// TEAM C: Jordan, Yufei, David, Shannon, Asher
+// Jordan, Yufei, David, Shannon, Asher
 // Frupal 
-// CS300
 // Jan 28, 2018
 #include"player.h"
 
